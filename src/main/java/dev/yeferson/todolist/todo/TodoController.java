a9297c0088908134545d3bf6a5d6ecbfc867f055
unknown <gaviriayeferson2@gmail.com>
2025-05-19 10:30:52 +0200

    first commit

---
+++ src/main/java/dev/yeferson/todolist/todo/TodoController.java
@@ -0,0 +1,14 @@
+package todo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todos")
+
+public class TodoController {
+
+
+
+}
